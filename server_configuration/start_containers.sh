444018dc16765eb49d0b52fb30d9ae5479166fe7
arthurmoraesrf <arthur.moraes@raizen.com>
2024-11-08 15:45:34 -0300

    feat: container docker /clima; iniciaizacao automatica de containers

---
+++ server_configuration/start_containers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Iniciando container Airflow MySQL..."
+docker-compose -f /WX2TB/Documentos/fontes/PMO/scripts_unificados/server_configuration/containers/airflow_mysql/docker-compose.yaml up -d
+
+echo "Iniciando container Nginx..."
+docker-compose -f /WX2TB/Documentos/fontes/PMO/scripts_unificados/server_configuration/containers/nginx/docker-compose-nginx.yaml up -d
+
+echo "Iniciando container OpenTelemetry Collector..."
+docker-compose -f /WX2TB/Documentos/fontes/PMO/scripts_unificados/server_configuration/containers/openTelemetryCollector/docker-compose.yml up -d
+
+echo "Iniciando container SFTP..."
+docker-compose -f /WX2TB/Documentos/fontes/PMO/scripts_unificados/server_configuration/containers/sftp/docker-compose.yml up -d
+
+echo "Iniciando container do aplicativo personalizado..."
+docker-compose -f /home/admin/jose/app/docker-compose.yaml up -d
