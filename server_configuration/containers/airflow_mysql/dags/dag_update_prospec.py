405da447ec8d626fc51823a906cdd939a896df24
joaoCalmon <joao.filho4@raizen.com>
2025-01-07 16:00:33 -0300

    feat: dag para rotina de atualização de estudos do prospec

---
+++ server_configuration/containers/airflow_mysql/dags/dag_update_prospec.py
@@ -0,0 +1,132 @@
+import sys
+import json
+import datetime
+from airflow.models.dag import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python import BranchPythonOperator, PythonOperator
+from airflow.providers.ssh.operators.ssh import SSHOperator
+
+
+sys.path.insert(1,"/WX2TB/Documentos/fontes")
+from PMO.scripts_unificados.apps.prospec.libs import update_estudo
+
+
+def trigger_task_sequence(**kwargs):
+    kwargs.get('dag_run').conf['ids_to_modify'] =  update_estudo.get_ids_to_modify()
+    print(kwargs.get('dag_run').conf['ids_to_modify'])
+    kwargs.get('dag_run').conf['ids_to_modify'] =  [22461]
+    print(kwargs.get('dag_run').conf['ids_to_modify'])
+
+    task_name = kwargs.get('dag_run').conf.get('external_params').get('task_to_execute')
+    return [task_name]
+
+with DAG(
+    dag_id='PROSPEC_UPDATER',
+    tags=["PROSPEC"],
+    start_date= datetime.datetime(2024, 4, 28),
+    catchup=False,
+    schedule=None,   
+    render_template_as_native_obj=True, 
+) as dag:
+    
+    inicio = BranchPythonOperator(
+        task_id='inicio',
+        python_callable=trigger_task_sequence,
+        provide_context=True,
+    )
+
+    revisao_cvu = DummyOperator(
+        task_id='revisao_cvu',
+    )
+    revisao_carga_dc = DummyOperator(
+        task_id='revisao_carga_dc',
+    )
+    revisao_eolica = DummyOperator(
+        task_id='revisao_eolica',
+    )
+    revisao_carga_nw = DummyOperator(
+        task_id='revisao_carga_nw',
+    )
+
+    cvu_dadger_decomp = PythonOperator(
+        task_id='cvu_dadger_decomp',
+        python_callable=update_estudo.update_cvu_dadger_dc_estudo,
+        provide_context=True,
+        op_kwargs={
+            "ano_referencia_cvu": '{{ dag_run.conf.get("external_params").get("ano_referencia_cvu")}}',
+            "mes_referencia_cvu": '{{ dag_run.conf.get("external_params").get("mes_referencia_cvu")}}',
+            "ids_to_modify":'{{ dag_run.conf.get("ids_to_modify") }}'
+            },
+        
+        
+    )
+
+    carga_dadger_decomp = PythonOperator(
+        task_id='carga_dadger_decomp',
+        python_callable=update_estudo.update_carga_dadger_dc_estudo,
+        provide_context=True,
+        op_kwargs={
+            "file_path": '{{ dag_run.conf.get("external_params").get("file_path")}}',
+            "ids_to_modify":'{{ dag_run.conf.get("ids_to_modify") }}'
+            },
+        
+    )
+
+    eolica_dadger_decomp = PythonOperator(
+        task_id='eolica_dadger_decomp',
+        python_callable=update_estudo.update_weol_dadger_dc_estudo,
+        provide_context=True,
+        op_kwargs={
+            "file_path": '{{ dag_run.conf.get("external_params").get("file_path")}}',
+            "ids_to_modify":'{{ dag_run.conf.get("ids_to_modify") }}'
+            },
+        
+
+    )
+
+    cvu_clast_newave = PythonOperator(
+        task_id='cvu_clast_newave',
+        python_callable=update_estudo.update_cvu_clast_nw_estudo,
+        provide_context=True,
+        op_kwargs={
+            "ano_referencia_cvu": '{{ dag_run.conf.get("external_params").get("ano_referencia_cvu")}}',
+            "mes_referencia_cvu": '{{ dag_run.conf.get("external_params").get("mes_referencia_cvu")}}',
+            "ids_to_modify":'{{ dag_run.conf.get("ids_to_modify") }}'
+            },
+        
+
+    )
+
+    carga_c_adic_newave = PythonOperator(
+        task_id='carga_c_adic_newave',
+        python_callable=update_estudo.update_carga_c_adic_nw_estudo,
+        provide_context=True,
+        op_kwargs={
+            "file_path": '{{ dag_run.conf.get("external_params").get("file_path")}}',
+            "ids_to_modify":'{{ dag_run.conf.get("ids_to_modify") }}'
+            },
+        
+
+    )
+
+    carga_sistema_newave = PythonOperator(
+        task_id='carga_sistema_newave',
+        python_callable=update_estudo.update_carga_sistema_nw_estudo,
+        provide_context=True,
+        op_kwargs={
+            "file_path": '{{ dag_run.conf.get("external_params").get("file_path")}}',
+            "ids_to_modify":'{{ dag_run.conf.get("ids_to_modify") }}'
+            },
+        
+    )
+
+    fim = DummyOperator(
+        task_id='fim',
+    )
+
+
+# Definindo dependências com base na decisão
+inicio >> revisao_cvu >> cvu_dadger_decomp >> cvu_clast_newave >> fim
+inicio >> revisao_carga_dc >> carga_dadger_decomp  >> fim
+inicio >> revisao_eolica >> eolica_dadger_decomp >> fim
+inicio >> revisao_carga_nw >> carga_c_adic_newave >> carga_sistema_newave >> fim
