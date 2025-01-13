e725292bc59d686a58b9469fbf77a30806b5e76d
arthurfmoraesrz <arthur.moraes@raizen.com>
2025-01-13 14:53:31 -0300

    merge com alteracoes do servidor

--- server_configuration/containers/airflow_mysql/dags/dag_produtos_webhook.py
+++ server_configuration/containers/airflow_mysql/dags/dag_produtos_webhook.py
@@ -6,6 +6,8 @@ from airflow.exceptions import AirflowException
 from airflow.models.dag import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python import BranchPythonOperator, PythonOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
 
 
 sys.path.insert(1,"/WX2TB/Documentos/fontes/")
@@ -42,16 +44,74 @@ def trigger_function(**kwargs):
     function_name = params.get('function_name')
     product_details = params.get('product_details')
     product_name = product_details.get('nome')
-
+    
     try:
         # Obtendo a função do módulo importado baseada no nome
         func = getattr(manipularArquivosShadow, function_name, None)
-        func(product_details)
+        aditional_params = func(product_details)
+        print(product_name)
+        
+        if product_name in ['Previsões de carga mensal e por patamar - NEWAVE','Carga por patamar - DECOMP']:
+            kwargs.get('dag_run').conf.update(aditional_params)	
+            return['trigger_external_dag']
+
     except Exception as e:
-        raise AirflowException(f"[ERROR] Não foi possivel executar a funcao!: {str(e)}")
-    
+        raise AirflowException(f"Erro ao chamar a função {function_name}. {e}")
+
     return ['fim']
     
+    
+with DAG(
+    dag_id='WEEBHOOK',
+    start_date=datetime.datetime(2024, 4, 28),
+    catchup=False,
+    schedule=None,
+    tags=['webhook'],
+    default_args={
+        'retries': 4,
+        'retry_delay': datetime.timedelta(minutes=5) 
+    },
+    render_template_as_native_obj=True, 
+
+) as dag:
+
+    # começo estrutura para rodar a sequencia das tarefas
+    inicio = BranchPythonOperator(
+        task_id='inicio',
+        python_callable=convert_to_clean_name,
+        provide_context=True,
+    )
+
+    trigger_updater_estudo = TriggerDagRunOperator(
+        task_id="trigger_external_dag",
+        trigger_dag_id='{{ dag_run.conf.get("trigger_dag_id")}}',
+        conf={'external_params': "{{dag_run.conf}}"},  
+        wait_for_completion=False,  
+        trigger_rule="none_failed_min_one_success",
+
+    )
+
+    fim = DummyOperator(
+        task_id='fim',
+        trigger_rule="none_failed_min_one_success",
+    )
+
+    for webhookProdut in PRODUCT_MAPPING.keys():
+
+        produtoEscolhido = BranchPythonOperator(
+            task_id=remover_acentos_e_caracteres_especiais(webhookProdut),
+            trigger_rule="none_failed_min_one_success",
+            python_callable = trigger_function,
+            provide_context=True,
+        )
+
+        inicio >> produtoEscolhido
+        produtoEscolhido >> trigger_updater_estudo >> fim
+        produtoEscolhido >> fim
+
+
+#=================================================================================================
+
 def deck_prev_eolica_semanal_patamares(**kwargs):
     params = kwargs.get('dag_run').conf
     manipularArquivosShadow.deck_prev_eolica_semanal_patamares(params)
@@ -68,41 +128,6 @@ def deck_prev_eolica_semanal_update_estudos(**kwargs):
 def gerar_produto(**kwargs):
     params = kwargs.get('dag_run').conf
     manipularArquivosShadow.enviar_tabela_comparacao_weol_whatsapp_email(params)
-    
-with DAG(
-    dag_id='WEEBHOOK',
-    start_date=datetime.datetime(2024, 4, 28),
-    catchup=False,
-    schedule=None,
-    tags=['webhook'],
-    default_args={
-        'retries': 4,
-        'retry_delay': datetime.timedelta(minutes=5) 
-    }
-) as dag:
-
-    # começo estrutura para rodar a sequencia das tarefas
-    inicio = BranchPythonOperator(
-        task_id='inicio',
-        python_callable=convert_to_clean_name,
-        provide_context=True,
-    )
-
-    fim = DummyOperator(
-        task_id='fim',
-        trigger_rule="none_failed_min_one_success",
-    )
-
-    for webhookProdut in PRODUCT_MAPPING.keys():
-
-        produtoEscolhido = BranchPythonOperator(
-            task_id=remover_acentos_e_caracteres_especiais(webhookProdut),
-            trigger_rule="none_failed_min_one_success",
-            python_callable = trigger_function,
-            provide_context=True,
-        )
-        inicio >> produtoEscolhido
-        produtoEscolhido >> fim
 
 with DAG(
     dag_id='webhook_deck_prev_eolica_semanal_weol',
