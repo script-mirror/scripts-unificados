4eb99552d0c86a0f7a9726dad9084aa806952071
joaoCalmon <jhony_calmo@hotmail.com>
2025-02-24 16:49:37 -0300

    feat(gerar_produtos): auth parameter for whatsapp api

--- apps/gerarProdutos/gerarProdutos.py
+++ apps/gerarProdutos/gerarProdutos.py
@@ -74,6 +74,7 @@ import rz_deck_dc_preliminar
 import rz_relatorio_bbce
 import rz_produtos_chuva
 
+from .utils import get_access_token
 
 def baixarProduto(data, produto):
 	""" Faz o download de produtos externos a WX
@@ -1438,12 +1439,18 @@ def enviar(parametros):
 				files={
 					"arquivo": (os.path.basename(fileWhats), open(fileWhats, "rb"))
 				}
-			response = requests.post(os.getenv('WHATSAPP_API'), data=fields, files=files)
+			response = requests.post(
+				os.getenv('WHATSAPP_API'), 
+				data=fields, 
+				files=files,
+				headers={
+					'Content-Type': 'application/json',
+					'Authorization': f'Bearer {get_access_token()}'
+					})
 			print("Status Code:", response.status_code)
 				
 
 			flagWhats = False
-    
 
 	elif parametros["produto"] == 'PREVISAO_GEADA':
 
