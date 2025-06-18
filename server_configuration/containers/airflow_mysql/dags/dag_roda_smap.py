307f0d07425a8ebe7704053c87ae8a85c758fd94
raizen-arthurmoraes <arthur.moraes2@raizen.com>
2025-06-18 17:08:28 -0300

    feat: rodando smap com repositorio novo

--- server_configuration/containers/airflow_mysql/dags/dag_roda_smap.py
+++ server_configuration/containers/airflow_mysql/dags/dag_roda_smap.py
@@ -1,18 +1,10 @@
-
-import sys
 import json
 import datetime
-
 from airflow import DAG
 from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.providers.standard.operators.bash import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-
-
-
-sys.path.insert(0, "/WX2TB/Documentos/fontes/PMO/raizen-power-trading-previsao-hidrologia/smap")
-from main import SMAP
-
 
 default_args = {
     'owner': 'airflow',
@@ -20,44 +12,14 @@ default_args = {
     'start_date': datetime.datetime(2024, 7, 10),
     'retries': 0,
     'retry_delay': datetime.timedelta(minutes=5),
-    
+
 }
 
-def create_smap_object(**kwargs):
 
-    # Se precisar inserir caminho no sys.path:
+def update_conf(**kwargs):
+    cenario = kwargs['ti'].xcom_pull(task_ids='run_smap')
+    kwargs.get('dag_run').conf['cenario'] = json.loads(cenario)
 
-    lista_modelos = kwargs["dag_run"].conf.get("modelos", [])
-    flag_estendido = kwargs["dag_run"].conf.get("prev_estendida", False)
-    modelos = [(item[0], item[1], datetime.datetime.strptime(item[2], '%Y-%m-%d').date()) for item in lista_modelos] 
-    
-    smap_operator = SMAP(modelos=modelos,flag_estendido=flag_estendido)
-
-    return smap_operator
-
-def build_arq_entrada(**kwargs):
-    """
-    Recupera o objeto SMAP via XCom e chama build_arq_entrada().
-    """
-    ti = kwargs['ti']
-    smap_operator = ti.xcom_pull(task_ids='create_smap_object')
-    smap_operator.build_arq_entrada()
-
-
-def import_vazao_prevista(**kwargs):
-    """
-    Recupera o objeto SMAP via XCom e chama import_vazao_prevista().
-    """
-    ti = kwargs['ti']
-    smap_operator = ti.xcom_pull(task_ids='create_smap_object')
-    id_dataviz_chuva = kwargs['dag_run'].conf.get('id_dataviz_chuva')
-    cenario = smap_operator.import_vazao_prevista(id_dataviz_chuva)
-    
-    if isinstance(cenario['dt_rodada'], datetime.date):
-        cenario['dt_rodada'] = cenario['dt_rodada'].isoformat()
-
-    kwargs.get('dag_run').conf['cenario'] = cenario
-    
 
 with DAG(
     dag_id='PREV_SMAP',
@@ -70,51 +32,42 @@ with DAG(
     catchup=False
 ) as dag:
 
-    
-    t_create_smap = PythonOperator(
-        task_id='create_smap_object',
-        python_callable=create_smap_object
+    t_run_smap = BashOperator(
+        task_id='run_smap',
+        bash_command='cd /WX2TB/Documentos/fontes/PMO/'
+                     'raizen-power-trading-smap && source env/bin/activate && '
+                     'python main.py {{ dag_run.conf.get("id_chuva") }}',
+        do_xcom_push=True,
     )
 
-    # Task 1
-    t_build_arq_entrada = PythonOperator(
-        task_id='build_arq_entrada',
-        python_callable=build_arq_entrada
-    )
-
-    t_run_smap = SSHOperator(
-        task_id='run_container',
-        ssh_conn_id='ssh_master',
-        command='cd /WX2TB/Documentos/fontes/PMO/raizen-power-trading-previsao-hidrologia/smap; docker-compose up',
-        get_pty=True,
-        conn_timeout = 36000,
-        cmd_timeout = 28800,
-    )
-
-    # Task 4
-    t_import_vazao_prevista = PythonOperator(
-        task_id='import_vazao_prevista',
-        python_callable=import_vazao_prevista
+    t_update_conf = PythonOperator(
+        task_id='update_conf',
+        python_callable=update_conf
     )
 
     t_run_previvaz = TriggerDagRunOperator(
         task_id="trigger_previvaz",
         trigger_dag_id='PREV_PREVIVAZ',
-        conf={'cenario': "{{ dag_run.conf.get('cenario')}}"},  
+        conf={'cenario': "{{ dag_run.conf.get('cenario')}}"},
         wait_for_completion=False,  
         trigger_rule="none_failed_min_one_success",
 
     )
-    
+
     atualizar_cache = SSHOperator(
         task_id='atualizar_cache',
         ssh_conn_id='ssh_master',
-        command=". /WX2TB/pythonVersions/myVenv38/bin/activate;cd /WX2TB/Documentos/fontes/PMO/scripts_unificados/apps/web_modelos/server/caches;python rz_cache.py import_ena_visualization_api dt_rodada {{ dag_run.conf.get('cenario')['dt_rodada'] }} id_nova_rodada {{ dag_run.conf.get('cenario')['id'] }} id_dataviz_chuva {{ dag_run.conf.get('id_dataviz_chuva') }}",
-        conn_timeout = None,
-        cmd_timeout = None,
+        command=". /WX2TB/pythonVersions/myVenv38/bin/activate;"
+                "cd /WX2TB/Documentos/fontes/PMO/scripts_unificados/"
+                "apps/web_modelos/server/caches;python rz_cache.py "
+                "import_ena_visualization_api dt_rodada "
+                "{{ dag_run.conf.get('cenario')['dt_rodada'] }} "
+                "id_nova_rodada {{ dag_run.conf.get('cenario')['id'] }} "
+                "id_dataviz_chuva {{ dag_run.conf.get('id_dataviz_chuva') }}",
+        conn_timeout=None,
+        cmd_timeout=None,
         get_pty=True,
         trigger_rule="all_done",
     )
-    
-    # Definindo a sequência
-    t_create_smap >> t_build_arq_entrada >> t_run_smap >> t_import_vazao_prevista >> atualizar_cache >> t_run_previvaz
+
+    t_run_smap >> atualizar_cache >> t_run_previvaz
