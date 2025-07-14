0eaefa9c58b0a24f2ee9de649a36f59cbb8db6c4
raizen-arthurmoraes <arthur.moraes2@raizen.com>
2025-07-14 10:22:41 -0300

    refact: utilizando api de estudos para pegar dados de cvu da ccee

--- server_configuration/containers/airflow_mysql/dags/dag_api_ccee.py
+++ server_configuration/containers/airflow_mysql/dags/dag_api_ccee.py
@@ -1,142 +1,176 @@
-import sys
+import os
 import datetime
 import requests as req
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
-from airflow.operators.python import BranchPythonOperator,PythonOperator
+from airflow.operators.python import BranchPythonOperator, PythonOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
+from middle.utils import get_auth_header
 from dotenv import load_dotenv
-import os
+load_dotenv(os.path.join(os.path.abspath(os.path.expanduser("~")), '.env'))
 
-load_dotenv(os.path.join(os.path.abspath(os.path.expanduser("~")),'.env'))
-URL_COGNITO = os.getenv("URL_COGNITO")
-CONFIG_COGNITO = os.getenv("CONFIG_COGNITO")
-PATH_PROJETO = os.getenv("PATH_PROJETO", "/WX2TB/Documentos/fontes/PMO")
+WHATSAPP_API = os.getenv("WHATSAPP_API")
 
-sys.path.insert(0, f"{PATH_PROJETO}/raizen-power-trading-api-ccee")
-from main import ApiCCEE
-from src.constants import MAPPING
-
-def get_access_token() -> str:
-    response = req.post(
-        URL_COGNITO,
-        data=CONFIG_COGNITO,
-        headers={'Content-Type': 'application/x-www-form-urlencoded'}
-    )
-    print(response.status_code, response.text)
-    return response.json()['access_token']
 
 def enviar_whatsapp_erro(context):
     task_instance = context['task_instance']
     dag_id = context['dag'].dag_id
     task_id = task_instance.task_id
-    
+
     msg = f"❌ Erro na DAG: *{dag_id}*\nTask: *{task_id}*"
     fields = {
         "destinatario": "Airflow",
         "mensagem": msg
     }
-    headers = {'accept': 'application/json', 'Authorization': f'Bearer {get_access_token()}'}
+    headers = get_auth_header()
     response = req.post(WHATSAPP_API, data=fields, headers=headers)
     print("Enviando log para whatsapp:", response.status_code)
-    
-default_args = {
-    'execution_timeout': datetime.timedelta(hours=8),
-    'retries': 2,
-    'retry_delay': datetime.timedelta(minutes=1),
-    'on_failure_callback': enviar_whatsapp_erro,
-}
 
-def check_cvu_status_processamento(title:str, data_atualizacao:datetime.datetime):
+
+def get_cvu_mapping():
+    res = req.get('https://tradingenergiarz.com/'
+                  'estudos-middle/api/ccee/cvu-mapping',
+                  headers=get_auth_header())
+    if res.status_code == 200:
+        return res.json()
+    else:
+        raise Exception("Erro ao obter mapeamento CVU:"
+                        f" {res.status_code} - {res.text}")
+
+
+def get_date_atualizacao(tipo_cvu: str):
+    res = req.get('https://tradingenergiarz.com/'
+                  f'estudos-middle/api/ccee/{tipo_cvu}/ultima-atualizacao',
+                  headers=get_auth_header())
+    if res.status_code == 200:
+        return res.json()
+    else:
+        raise Exception("Erro ao ultima atualizacao cvu:"
+                        f" {res.status_code} - {res.text}")
+
+
+def get_cvu_data(tipo_cvu: str):
+    res = req.get('https://tradingenergiarz.com/'
+                  f'estudos-middle/api/ccee/{tipo_cvu}',
+                  headers=get_auth_header())
+    if res.status_code == 200:
+        return res.json()
+    else:
+        raise Exception("Erro ao obter dados CVU:"
+                        f" {res.status_code} - {res.text}")
+
+
+def post_cvu(body: dict, tipo_cvu: str):
+    url = 'https://tradingenergiarz.com/api/v2/decks'
+    if tipo_cvu == 'merchant':
+        url = url + '/merchant'
+    res = req.post(url,
+                   json=body,
+                   headers=get_auth_header())
+    if res.status_code == 201:
+        return res.json()
+    else:
+        raise Exception("Erro ao postar dados CVU:"
+                        f" {res.status_code} - {res.text}")
+
+def check_cvu_status_processamento(tipo_cvu: str, data_atualizacao: str):
     res = req.get("https://tradingenergiarz.com/api/v2/decks/check-cvu", params={
-        'title': title,
-        'data_atualizacao': data_atualizacao.strftime('%Y-%m-%dT%H:%M:%S'),
+        'tipo_cvu': tipo_cvu,
+        'data_atualizacao': data_atualizacao,
         },
-        headers={'Authorization': f'Bearer {get_access_token()}'}
+        headers=get_auth_header()
     )
     print(res.status_code, res.text)
-    print(title)
     print(data_atualizacao.strftime('%Y-%m-%dT%H:%M:%S'))
     if res.status_code == 404:
         res = req.post("https://tradingenergiarz.com/api/v2/decks/check-cvu", json={
-                'tipo_cvu': title,
-                'data_atualizacao': data_atualizacao.strftime('%Y-%m-%dT%H:%M:%S'),
+                'tipo_cvu': tipo_cvu,
+                'data_atualizacao': data_atualizacao,
                 'status': 'processando',
                 },
-                headers={'Authorization': f'Bearer {get_access_token()}'}     
+                headers=get_auth_header()     
             )
         print(res.status_code, res.text)
         return res.json()
     return res.json()
+
 
 def mark_cvu_as_processed(ids_cvu:list):
     for id_check_cvu in ids_cvu:
         res = req.patch(f"https://tradingenergiarz.com/api/v2/decks/check-cvu/{id_check_cvu}/status", params={
             'status': 'processado',
             },
-            headers={'Authorization': f'Bearer {get_access_token()}'}
+            headers=get_auth_header()
         )
         print(res.status_code, res.text)
     return res.json()
 
 def check_atualizacao(**kwargs):
 
-    titles_to_search = kwargs.get('dag_run').conf.get('titles_to_search',[])
+    cvus_to_search = kwargs.get('dag_run').conf.get('cvus_to_search',[])
     ids_to_modify = kwargs.get('dag_run').conf.get('ids_to_modify',[])
 
     ultima_data_atualizacao = datetime.datetime.min
     ids_cvu_nao_processados = []
-    if not titles_to_search:
-        for title_name in MAPPING.keys():
-            dt_atualizacao = ApiCCEE(title_name).get_date_atualizacao(title_name)
+    tipos_cvu = get_cvu_mapping()['tipos_cvu']
+    if not cvus_to_search:
+        for tipo_cvu in tipos_cvu:
+            dt_atualizacao = get_date_atualizacao(tipo_cvu)['data_atualizacao']
             check_cvu = check_cvu_status_processamento(
-                title_name, 
+                tipo_cvu,
                 dt_atualizacao
             )
             if check_cvu.get('status') == 'processando':
                 ids_cvu_nao_processados.append(check_cvu.get('id'))
                 if dt_atualizacao:
-                    titles_to_search.append(title_name)
+                    cvus_to_search.append(tipo_cvu)
                     if dt_atualizacao > ultima_data_atualizacao:
                         ultima_data_atualizacao = dt_atualizacao
         kwargs.get('dag_run').conf.update()
     
-    if titles_to_search:
-        fontes_to_search = [title.replace("custo_variavel_unitario_","CCEE_") for title in titles_to_search]
+    if cvus_to_search:
+        fontes_to_search = [f"CCEE_{title}" for title in cvus_to_search]
         
         kwargs.get('dag_run').conf.update(
-            {   'titles_to_search':titles_to_search, 
+            {   'cvus_to_search':cvus_to_search, 
                 'fontes_to_search': fontes_to_search,
                 "dt_atualizacao": ultima_data_atualizacao.strftime('%Y-%m-%d'),
                 'task_to_execute': 'revisao_cvu',
                 'ids_to_modify': ids_to_modify
             })
         kwargs['ti'].xcom_push(key='ids_cvu_nao_processados', value=ids_cvu_nao_processados)
-        kwargs['ti'].xcom_push(key='titles_to_search', value=titles_to_search)
+        kwargs['ti'].xcom_push(key='cvus_to_search', value=cvus_to_search)
 
-        return "create_ccee_instance"
+        return "export_data_to_db"
     return "end_task"
 
 
-def create_ccee_instance(**kwargs):
-
-    titles_to_search = kwargs['ti'].xcom_pull(task_ids='check_atualizacao', key='titles_to_search')
-    ccee_operator = ApiCCEE(titles_to_search)
-    return ccee_operator
-
 def export_data_to_db(**kwargs):
-    ccee_operator = kwargs['ti'].xcom_pull(task_ids='create_ccee_instance')
-    ids_cvu = kwargs['ti'].xcom_pull(task_ids='check_atualizacao', key='ids_cvu_nao_processados')
-    ccee_operator.export_data()
+    cvus_to_search = kwargs['ti'].xcom_pull(
+        task_ids='check_atualizacao',
+        key='cvus_to_search'
+    )
+    ids_cvu = kwargs['ti'].xcom_pull(
+        task_ids='check_atualizacao',
+        key='ids_cvu_nao_processados'
+    )
+    for tipo_cvu in cvus_to_search:
+        post_cvu(get_cvu_data(tipo_cvu), tipo_cvu)
     mark_cvu_as_processed(ids_cvu)
 
 
 with DAG(
     'API_CCEE_CVU',
-    default_args=default_args,
+    default_args={
+        'execution_timeout': datetime.timedelta(hours=8),
+        'retries': 2,
+        'retry_delay': datetime.timedelta(minutes=1),
+        'on_failure_callback': enviar_whatsapp_erro,
+    },
     description='Consulta a api da ccee',
-    schedule_interval = '*/5 7-23 * * *', 
-    tags=["PROSPEC","CCEE","CVU"],
+    schedule_interval='*/5 7-23 * * *',
+    tags=["PROSPEC", "CCEE", "CVU"],
     start_date=datetime.datetime(2024, 4, 28),
     catchup=False,
     render_template_as_native_obj=True, 
@@ -146,12 +180,6 @@ with DAG(
     check_atualizacao = BranchPythonOperator(
         task_id='check_atualizacao',
         python_callable=check_atualizacao,
-        provide_context=True,
-    )
-
-    create_ccee_instance = PythonOperator(
-        task_id='create_ccee_instance',
-        python_callable=create_ccee_instance,
         provide_context=True,
     )
 
@@ -173,6 +201,5 @@ with DAG(
         task_id='end_task',
     )
 
-
-    check_atualizacao >> create_ccee_instance >> export_data_to_db >> trigger_updater_estudo >> end_task
+    check_atualizacao >> export_data_to_db >> trigger_updater_estudo >> end_task
     check_atualizacao >> end_task
