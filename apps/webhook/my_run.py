3b6becebaff98c12746b0026b89f3d5094b1d2a2
raizen-arthurmoraes2 <arthur.moraes2@raizen.com>
2025-09-09 15:19:52 -0300

    refact: retirando limites de intercambio

--- apps/webhook/my_run.py
+++ apps/webhook/my_run.py
@@ -87,8 +87,8 @@ PRODUCT_MAPPING = {
     'DECK NEWAVE DEFINITIVO': {"funcao": None, "id_dag": "DECKS_NEWAVE"}, 
     "DECKS DA PREVISÃO DE GERAÇÃO EÓLICA SEMANAL WEOL-SM": {"funcao": "deck_prev_eolica_semanal_weol", "id_dag": None},
     "Preliminar - Relatório Mensal de Limites de Intercâmbio": {"funcao": "relatorio_limites_intercambio", "id_dag": None},
-    "Relatório Mensal de Limites de Intercâmbio para o Modelo DECOMP": {"funcao": "relatorio_limites_intercambio", "id_dag": None},
-    "Notas Técnicas - Medio Prazo": {"funcao": "notas_tecnicas_medio_prazo", "id_dag": None},
+    # "Relatório Mensal de Limites de Intercâmbio para o Modelo DECOMP": {"funcao": "relatorio_limites_intercambio", "id_dag": None},
+    # "Notas Técnicas - Medio Prazo": {"funcao": "notas_tecnicas_medio_prazo", "id_dag": None},
 }
 
 
